13d0921d0a6385ba857d65d9872abf5663869cce
JIMMY Online <jimmy.is.now.online@gmail.com>
2019-04-07 14:34:36 -0500

    Fix(architecture.component): remove unused interface onInit

--- src/app/architecture/architecture/architecture.component.ts
+++ src/app/architecture/architecture/architecture.component.ts
@@ -1,15 +1,12 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'ja-architecture',
   templateUrl: './architecture.component.html',
   styleUrls: ['./architecture.component.scss']
 })
-export class ArchitectureComponent implements OnInit {
+export class ArchitectureComponent {
 
   constructor() { }
-
-  ngOnInit() {
-  }
 
 }
