14fb21055f54e2e73a11811181ab715bba4d40af
JIMMY Online <jimmy.is.now.online@gmail.com>
2019-04-07 17:18:27 -0500

    Fix(intro.component): remove one nested level for the intro component

--- src/app/reactive/reactive-routing.module.ts
+++ src/app/reactive/reactive-routing.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-import { IntroComponent } from './intro/intro/intro.component';
+import { IntroComponent } from './intro/intro.component';
 
 const routes: Routes = [{
   path: '',
--- src/app/reactive/reactive.module.ts
+++ src/app/reactive/reactive.module.ts
@@ -4,7 +4,7 @@ import { CommonModule } from '@angular/common';
 import { ReactiveRoutingModule } from './reactive-routing.module';
 import { ReactiveComponent } from './reactive/reactive.component';
 import { CommonsModule } from '../commons/commons.module';
-import { IntroComponent } from './intro/intro/intro.component';
+import { IntroComponent } from './intro/intro.component';
 
 @NgModule({
   declarations: [ReactiveComponent, IntroComponent],
